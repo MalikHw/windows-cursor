429a5237dcf9a42f24e1f1dc83989968cd1785f7
MalikHw47 <123307564+MalikHw@users.noreply.github.com>
2026-04-14 18:48:55 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,34 @@
 #include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+#ifdef GEODE_IS_WINDOWS
+#include <windows.h>
+
+// cache the cursors once so we don't reload them every frame
+static HCURSOR s_handCursor = nullptr;
+static HCURSOR s_arrowCursor = nullptr;
+
+static void ensureCursors() {
+    if (!s_handCursor)
+        s_handCursor = LoadCursorW(nullptr, IDC_HAND);
+    if (!s_arrowCursor)
+        s_arrowCursor = LoadCursorW(nullptr, IDC_ARROW);
+}
+
+// hook CCMenuItem::selected(), started when the cursor hovers a button
+class $modify(CCMenuItem) {
+    virtual void selected() {
+        CCMenuItem::selected();
+        ensureCursors();
+        SetCursor(s_handCursor);
+    }
+
+    virtual void unselected() {
+        CCMenuItem::unselected();
+        ensureCursors();
+        SetCursor(s_arrowCursor);
+    }
+};
+
+#endif // GEODE_IS_WINDOWS
