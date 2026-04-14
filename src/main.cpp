7b3752a4ae2c1aaf3e55df904744e6f49c73ffef
MalikHw47 <123307564+MalikHw@users.noreply.github.com>
2026-04-14 23:00:43 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,40 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CCMenuItem.hpp>
-
 using namespace geode::prelude;
 
 #ifdef GEODE_IS_WINDOWS
 #include <windows.h>
-#include "GLFW/glfw3.h"
-#define GLFW_EXPOSE_NATIVE_WIN32
-#include "GLFW/glfw3native.h"
 
-static GLFWcursor* s_handCursor = nullptr;
-static GLFWcursor* s_arrowCursor = nullptr;
+static HCURSOR s_handCursor = nullptr;
+static HCURSOR s_arrowCursor = nullptr;
+static bool s_useHand = false;
+static WNDPROC s_origWndProc = nullptr;
 
-static GLFWwindow* getGLFWWindow() {
-    auto view = cocos2d::CCEGLView::sharedOpenGLView();
-    return view->getWindow(); // returns the GLFWwindow*
+LRESULT CALLBACK hookedWndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+    if (msg == WM_SETCURSOR) {
+        SetCursor(s_useHand ? s_handCursor : s_arrowCursor);
+        return TRUE; // prevents Windows from resetting it
+    }
+    return CallWindowProcW(s_origWndProc, hwnd, msg, wp, lp);
 }
 
-static void ensureCursors() {
-    if (!s_handCursor) {
-        HCURSOR hHand = LoadCursor(nullptr, IDC_HAND);
-        // create a GLFW cursor from the win32 handle isn't directly possible,
-        // so use standard GLFW cursor shapes instead
-        s_handCursor  = glfwCreateStandardCursor(GLFW_HAND_CURSOR);
-        s_arrowCursor = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
-    }
+$execute {
+    s_handCursor  = LoadCursor(nullptr, IDC_HAND);
+    s_arrowCursor = LoadCursor(nullptr, IDC_ARROW);
+
+    HWND hwnd = GetActiveWindow();
+    s_origWndProc = (WNDPROC)SetWindowLongPtrW(hwnd, GWLP_WNDPROC, (LONG_PTR)hookedWndProc);
 }
 
 class $modify(CCMenuItem) {
     virtual void selected() {
         CCMenuItem::selected();
-        ensureCursors();
-        glfwSetCursor(getGLFWWindow(), s_handCursor);
+        s_useHand = true;
     }
 
     virtual void unselected() {
         CCMenuItem::unselected();
-        ensureCursors();
-        glfwSetCursor(getGLFWWindow(), s_arrowCursor);
+        s_useHand = false;
     }
 };
 
